c54050f2d9635e45c43697a4fab70d0b8a471783
Chris Penepent <chris.penepent@gmail.com>
2015-11-22 14:15:20 -0500

    added business layer login method

--- src/BLFaculty.java
+++ src/BLFaculty.java
@@ -1,4 +1,9 @@
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
 
 public class BLFaculty extends DLFaculty {
 
@@ -7,18 +12,23 @@ public class BLFaculty extends DLFaculty {
         super(ID);
     }
    
-   
-   public Login(String emai, String pwd) {
-   
-   
-   // hashing function ....
-   
-   // call DLFaculty Login
-   
-   
-   
-   }
-
-
-
+   public boolean login(String email, String pw) {
+		MessageDigest md;
+		byte[] hashedBytes = new byte[0];
+		try {
+			md = MessageDigest.getInstance("MD5");
+			hashedBytes = md.digest(pw.getBytes("UTF-8"));
+		} catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+			System.out.println("OOPSIE");
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (byte b : hashedBytes) {
+			sb.append(String.format("%02X", b));
+		}
+		String hashedPw = sb.toString();
+		String storedPw = DLFaculty.login(email);
+		return hashedPw.equals(storedPw);
+	}
+ 
 }
