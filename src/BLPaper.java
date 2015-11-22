61f931169eceaf55cb54b34765fc28e1abd9b5ec
kshaw08 <katie@192.168.2.100>
2015-11-22 12:53:58 -0500

    Katie changes: Updated my DLFaculty & DLPaper methods, created constructors in BLPaper and BLFaculty

--- src/BLPaper.java
+++ src/BLPaper.java
@@ -1,15 +1,17 @@
 public class BLPaper extends DLPaper {
 
-
+    public BLPaper(int ID){
+        super(ID);
+    }
+    
+    public BLPaper(int ID, String title, String pAbstract, String citation){
+        super(ID, title, pAbstract, citation);
+    }
       
 
-
 }
 
-public BLPaper(){
 
-
-}
 
 
 
