e3c4b4a3ae5042ac6ff0984eec2ee228cb5998a1
Chris Penepent <chris.penepent@gmail.com>
2015-12-05 16:54:49 -0500

    added author comment at the top

--- src/MainMenuUI.java
+++ src/MainMenuUI.java
@@ -6,16 +6,10 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.Document;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 
 /**
  *
- * @author Chris
+ * @author Group 2: Chris Penepent, Katherine Shaw, Fahad Alotaibi, Nazar Al-Wattar
  */
 public class MainMenuUI extends javax.swing.JFrame {
 
