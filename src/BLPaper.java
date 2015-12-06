fe4ebe219c670029a0d2895db928d6f1fa8b0e4c
Fahad Alotaibi <fahd_o@hotmail.com>
2015-12-06 18:08:11 -0500

    Merge origin/master
    Conflicts:
    	LogFile.txt
    	nbproject/project.properties

--- src/BLPaper.java
+++ src/BLPaper.java
@@ -1,9 +1,11 @@
 public class BLPaper extends DLPaper {
 
+    /* Default Constructor */
     public BLPaper(){
         
     }
     
+    /*Pramaterized Constructor*/
     public BLPaper(int ID){
         super(ID);
     }
@@ -11,7 +13,7 @@ public class BLPaper extends DLPaper {
     public BLPaper(int ID, String title, String pAbstract, String citation){
         super(ID, title, pAbstract, citation);
     }
-    
+        /* Get paper's title* to combobox*/
 	@Override
 	public String toString() {
 		return title;
